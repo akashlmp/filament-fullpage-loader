3324fb8cb218083cd999234c99c3f1fb1da6da68
Akash <akashkumarmaurya65@gmail.com>
2025-07-15 16:00:43 +0530

    Initial release

---
+++ src/Providers/FullPageLoaderServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+namespace Akash\FullPageLoader\Providers;
+
+use Filament\Support\Facades\FilamentView;
+use Illuminate\Support\ServiceProvider;
+
+class FullPageLoaderServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'fullpage-loader');
+        FilamentView::registerRenderHook(
+            'body.end',
+            fn() => view('fullpage-loader::components.full-page-loader')
+        );
+    }
+
+    public function register(): void
+    {
+    }
+}
